053cb7fda6ad2ba1c76abcc09a6e7d9707cbe181
stebomurkn420 <stevn4127@gmail.com>
2019-07-02 11:58:42 -0400

    Update fingerprint to PQ3B.190705.003

--- bootleg_sargo.mk
+++ bootleg_sargo.mk
@@ -43,8 +43,8 @@ PRODUCT_MANUFACTURER := Google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=sargo \
-    BUILD_FINGERPRINT=google/sargo/sargo:9/PD2A.190115.032/5340326:user/release-keys \
-    PRIVATE_BUILD_DESC="sargo-user 9 PD2A.190115.032 5340326 release-keys"
+    BUILD_FINGERPRINT=google/sargo/sargo:9/PQ3B.190705.003/5622519:user/release-keys \
+    PRIVATE_BUILD_DESC="sargo-user 9 PQ3B.190705.003 5622519 release-keys"
 
 $(call inherit-product-if-exists, vendor/google/sargo/sargo-vendor.mk)
 $(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
